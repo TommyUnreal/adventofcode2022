17c41dd225f473233fc9c3a95cdbfac9708c7f51
Tomáš Smolík <g.tomas.smolik@gmail.com>
2022-12-10 07:17:01 +0100

    Day 10: Cathode-Ray Tube

---
+++ 10/puzzle_1.py
@@ -0,0 +1,38 @@
+import os
+from dataclasses import dataclass
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+@dataclass
+class Instruction():
+    cycle: int = 0
+    add: int = 0
+
+    def __lt__(self, other):
+        return self.cycle < other.cycle
+
+if __name__ == "__main__":
+    with open(os.path.join(__location__,"input.txt")) as file:
+        lines = [line for line in (l.strip() for l in file)]
+
+        register = 1
+        a_sum = 0
+        operations = []
+        cycle = 1
+
+        for i, line in enumerate(lines):
+            if "noop" in line:
+                cycle += 1
+                operations.append(Instruction(cycle, 0))
+            if "addx " in line:
+                cycle += 2
+                operations.append(Instruction(cycle, int(line.split(" ")[1])))
+
+        for cycle in range(1,221): 
+            for i in sorted(operations):
+                if cycle == i.cycle:
+                    register += i.add
+            if cycle in [20, 60, 100, 140, 180, 220]:
+                    print(register, cycle)
+                    a_sum = a_sum + register*cycle
+        print(a_sum)
